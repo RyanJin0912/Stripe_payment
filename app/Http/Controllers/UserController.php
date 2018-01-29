c34d90273c130250951848ae4ceed7908b0ccd4e
RyanJin <a0988254326@gmail.com>
2018-01-29 22:28:29 +0800

    1.Controller divided Auth, guest 2.Auth::guard  Auth::login  Auth::logout  Auth::check()

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class UserController extends Controller
+{
+    public function getSignup()
+    {
+            return view('user.signup');
+    }//getSignup(user.signup)
+
+
+    public function postSignup(Request $request)
+    {
+        $this->validate($request,[
+            'email'=>'email|required|unique:users',
+            'password'=>'required|min:4'
+        ]); // if push enter the email and password , will got the validate
+
+
+        $user = new User([
+            'email'=>$request->input('email'),
+            'password'=>$request->input('password')
+        ]);// Enter the email and password by User
+
+
+        $user->save();
+
+                Auth::login($user);
+
+
+            return redirect()->route('user.profile');
+    }
+    //1.postSignup($request redirect()->route('product.index'))註冊後回到product.index
+    //2.Auth(user)使用者認證後到profile()
+
+
+    public function getSignin()
+    {
+        return view('user.signin');
+    }//getSignin(user.signin)
+
+
+    public function postSignin(Request $request)
+    {
+        $this->validate($request,[
+            'email'=>'email|required',
+            'password'=>'required|min:4'
+        ]);
+
+
+        if(Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('email')]))
+        {
+                    return redirect()->route('user.profile');
+        }//user.profile
+
+                return redirect()->back();
+
+    }
+    //postSignin(user.profile)登入認證
+
+
+    public function getProfile()
+    {
+        return view('user.profile');
+    }
+    //getProfile('getProfile')
+
+
+    public function getLogout()
+    {
+        Auth::logout();
+        return redirect()->back();
+    }//getLogout(->back)登出後重新back
+
+
+
+
+}
